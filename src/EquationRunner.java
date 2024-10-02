a7157299f46acca182c66b40d94f4c195103b229
BT_2S14_22 <email@>
2024-10-02 10:00:16 -0400

    Initial commit

---
+++ src/EquationRunner.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class EquationRunner {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter your first Coordinate Point: ");
+        System.out.print("Enter your second Coordinate Point: ");
+    }
+}
---
+++ src/LinearEquation.java
@@ -0,0 +1,36 @@
+public class LinearEquation {
+    private int s_Int;
+    private int x;
+    private int y;
+
+    public StringToInt(s_Int){
+        this.s_Int = s_Int;
+    }
+
+    public LinearEquation(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    // Create all strings
+    public String toString(){
+
+    }
+
+    public int slope(){
+
+    }
+
+    public double distance(){
+
+    }
+
+    public String slopeEquation(){
+
+    }
+
+
+    public int findY(){
+
+    }
+}
